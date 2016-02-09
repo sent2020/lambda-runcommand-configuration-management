58cd550fe3e9673d514a94c0b96ba320e451efc3
Josh Campbell <joshcb@amazon.com>
2016-02-09 16:45:07 -0600

    lambda function for triggering runcommand

--- lambda/functions/trigger_run_command/main.py
+++ lambda/functions/trigger_run_command/main.py
@@ -1,50 +1,33 @@
 """
-Validates HTTP Status of Environment
+Initiates RunCommand
 joshcb@amazon.com
-v1.0.1
+v1.0.0
 """
 from __future__ import print_function
 import logging
-from urllib2 import urlopen, URLError, HTTPError
 import boto3
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-def codepipeline_sucess(codepipeline, job_id):
-    """
-    Puts CodePipeline Sucess Result
-    """
-    codepipeline.put_job_success_result(jobId=job_id)
-
-def codepipeline_failure(codepipeline, job_id, message):
-    """
-    Puts CodePipeline Failure Result
-    """
-    codepipeline.put_job_failure_result(
-        jobId=job_id,
-        failureDetails={'type': 'JobFailed', 'message': message}
-    )
-
 def handle(event, context):
     """
     Lambda main handler
     """
-    codepipeline = boto3.client('codepipeline')
-    job_id = event['CodePipeline.job']['id']
-    job_data = event['CodePipeline.job']['data']
-    url = job_data['actionConfiguration']['configuration']['UserParameters']
-    try:
-        request = urlopen(url)
-        response_code = request.getcode()
-    except HTTPError as err:
-        LOGGER.error("Request failed: %d %s", err.code, err.reason)
-        codepipeline_failure(codepipeline, job_id, err.reason)
-    except URLError as err:
-        LOGGER.error("Server connection failed: %s", err.reason)
-        codepipeline_failure(codepipeline, job_id, err.reason)
-
-    if response_code == 200:
-        codepipeline_sucess(codepipeline, job_id)
-    else:
-        codepipeline_failure(codepipeline, job_data, ("Returned %d", response_code))
+    client = boto3.client('ssm')
+    client.send_command(
+        InstanceIds=[
+            'i-5f0941ec',
+            'i-011159b2'
+        ],
+        DocumentName='AWS-RunShellScript',
+        TimeoutSeconds=60,
+        Parameters={
+            'commands': [
+                'cd /tmp',
+                'git clone https://github.com/irlrobot/garlc.git',
+                'ansible-playbook -i "localhost," -c local garlc/ansible/playbook.yml'
+            ],
+            'executionTimeout': ['120']
+        }
+    )
