e55c5390a5db551090d101a41d44f5c557549640
Josh Campbell <joshcb@amazon.com>
2016-02-12 12:50:43 -0600

    adds todo to fix manually running the lambda

--- lambda/functions/trigger_run_command/main.py
+++ lambda/functions/trigger_run_command/main.py
@@ -102,6 +102,10 @@ def handle(event, context):
     try:
         job_id = event['CodePipeline.job']['id']
     except KeyError as err:
+        # TODO
+        # Better handle manual lambda invocations
+        # This will cause a ParamValidationError
+        job_id = 1
         LOGGER.error("Could not retrieve CodePipeline Job ID!\n%s", err)
 
     instance_ids = find_instances()
