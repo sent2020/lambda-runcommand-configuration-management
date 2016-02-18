c13b6e468edf64abe6443b5a045c72389e32709f
Josh Campbell <joshcb@amazon.com>
2016-02-17 16:44:26 -0800

    first test for lambda function :zap:

---
+++ lambda/functions/trigger_run_command/test_main.py
@@ -0,0 +1,18 @@
+"""
+Unit Tests for trigger_run_command Lambda function
+"""
+from main import ssm_commands
+from freezegun import freeze_time
+
+@freeze_time("2016-01-01")
+
+def test_ssm_commands():
+    artifact = 'bucket/test/key'
+    commands = [
+    'aws configure set s3.signature_version s3v4',
+    'aws s3 cp bucket/test/key /tmp/20151231160000.zip --quiet',
+    'unzip -qq /tmp/20151231160000.zip -d /tmp/20151231160000',
+    'bash /tmp/20151231160000/generate_inventory_file.sh',
+    'ansible-playbook -i "/tmp/inventory" /tmp/20151231160000/ansible/playbook.yml'
+    ]
+    assert ssm_commands(artifact) == commands
