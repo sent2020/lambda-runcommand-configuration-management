5fc35d793d4c45b69772c147904a95a96b54a233
Josh Campbell <joshcb@amazon.com>
2016-02-17 22:36:27 -0800

    adds more tests, some are failing :cry: i suck at python testing

--- lambda/functions/trigger_run_command/test_main.py
+++ lambda/functions/trigger_run_command/test_main.py
@@ -2,9 +2,16 @@
 Unit Tests for trigger_run_command Lambda function
 """
 import pytest
+import botocore
+from mock import MagicMock, Mock
 from main import log_event_and_context
 from main import find_artifact
 from main import ssm_commands
+from main import codepipeline_success
+from main import codepipeline_failure
+from main import find_instances
+from main import send_run_command
+from main import handle
 from freezegun import freeze_time
 
 def test_log_event_and_context():
@@ -55,3 +62,57 @@ def test_ssm_commands():
         'ansible-playbook -i "/tmp/inventory" /tmp/20160101000000/ansible/playbook.yml'
     ]
     assert ssm_commands(artifact) == commands
+
+def test_codepipeline_success(monkeypatch):
+    """
+    Test the codepipeline_success function with valid data
+    """
+    boto3 = MagicMock()
+    monkeypatch.setattr('boto3.client', MagicMock(return_value=boto3))
+    monkeypatch.setattr('boto3.client.put_job_success_result', MagicMock(return_value=True))
+    assert codepipeline_success(1) == True
+
+def test_codepipeline_success_invalid(monkeypatch):
+    """
+    Test the codepipeline_success function with invalid data
+    """
+    boto3 = MagicMock()
+    monkeypatch.setattr('boto3.client', MagicMock(return_value=boto3))
+    monkeypatch.setattr('boto3.client.put_job_success_result', Mock(side_effect=botocore.exceptions.ClientError))
+    with pytest.raises(botocore.exceptions.ClientError):
+        assert codepipeline_success(1)
+
+def test_codepipeline_failure(monkeypatch):
+    """
+    Test the codepipeline_failure function with valid data
+    """
+    boto3 = MagicMock()
+    monkeypatch.setattr('boto3.client', MagicMock(return_value=boto3))
+    monkeypatch.setattr('boto3.client.put_job_failure_result', MagicMock(return_value=True))
+    assert codepipeline_failure(1, 'blah') == True
+
+def test_codepipeline_failure_invalid(monkeypatch):
+    """
+    Test the codepipeline_failure function with invalid data
+    """
+    boto3 = MagicMock()
+    monkeypatch.setattr('boto3.client', MagicMock(return_value=boto3))
+    monkeypatch.setattr('boto3.client.put_job_success_result', Mock(side_effect=botocore.exceptions.ClientError))
+    with pytest.raises(botocore.exceptions.ClientError):
+        assert codepipeline_failure(1, 'blah')
+
+def test_find_instances(monkeypatch):
+    """
+    Test the find_instances function without errors
+    """
+    boto3 = MagicMock()
+    instances = {
+        'Reservations': [{
+            'Instances': [{
+                'InstanceId': 'abcdef-12345'
+            }]
+        }]
+    }
+    monkeypatch.setattr('boto3.client', MagicMock(return_value=boto3))
+    monkeypatch.setattr('boto3.client.describe_instances', MagicMock(return_value=instances))
+    assert find_instances() == 'abcdef-12345'
