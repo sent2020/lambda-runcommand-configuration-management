7122de01a386f45dafef5344831a1db6de9a4801
Josh Campbell <joshcb@amazon.com>
2016-03-07 17:31:23 -0600

    adds ssm policy using builtin aws policy, renames some tf variables

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,12 +1,19 @@
-resource "aws_iam_instance_profile" "subiaco" {
-    name = "subiaco_instance_profile"
-    roles = ["${aws_iam_role.subiaco.name}"]
+resource "aws_iam_instance_profile" "profile" {
+  name = "subiaco_instance_profile"
+  roles = ["${aws_iam_role.role.name}"]
 }
 
-resource "aws_iam_role" "subiaco" {
-    name = "subiaco_instance_role"
-    path = "/"
-    assume_role_policy = <<EOF
+# Use the builtin AmazonEC2RoleforSSM
+resource "aws_iam_policy_attachment" "policy" {
+    name = "attach_ssm_policy"
+    roles = ["${aws_iam_role.role.name}"]
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_role" "role" {
+  name = "subiaco_instance_role"
+  path = "/"
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -17,78 +24,11 @@ resource "aws_iam_role" "subiaco" {
       },
       "Effect": "Allow",
       "Sid": ""
-    },
-    {
-    "Effect": "Allow",
-    "Action": [
-        "ssm:DescribeAssociation",
-        "ssm:GetDocument",
-        "ssm:ListAssociations",
-        "ssm:UpdateAssociationStatus",
-        "ssm:UpdateInstanceInformation"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2messages:AcknowledgeMessage",
-        "ec2messages:DeleteMessage",
-        "ec2messages:FailMessage",
-        "ec2messages:GetEndpoint",
-        "ec2messages:GetMessages",
-        "ec2messages:SendReply"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "cloudwatch:PutMetricData"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstanceStatus"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ds:CreateComputer",
-        "ds:DescribeDirectories"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:AbortMultipartUpload",
-        "s3:ListMultipartUploadParts",
-        "s3:ListBucketMultipartUploads"
-      ],
-      "Resource": "*"
     }
   ]
 }
 EOF
 }
-output "role_arn" {
-  value = "${aws_iam_role.subiaco.arn}"
+output "profile_name" {
+  value = "${aws_iam_instance_profile.profile.name}"
 }
