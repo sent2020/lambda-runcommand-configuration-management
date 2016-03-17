e9c81e8736d7eccf3f7b7e2e5638e7e7749e8512
Josh Campbell <joshcb@amazon.com>
2016-03-17 16:58:12 -0500

    removes some comments :grin:

--- generate_inventory_file.sh
+++ generate_inventory_file.sh
@@ -17,11 +17,7 @@ tags_no_whitespace="$(echo -e "${tags}" | tr -d '[[:space:]]')"
 # Wipe out existing file :fire:
 printf '' > /tmp/inventory
 
-# Sweet mother of... what is this witchcraft?!?!
-# I literally do not understand how this works
 # http://stackoverflow.com/questions/10586153/split-string-into-an-array-in-bash
-# http://timmurphy.org/2012/03/09/convert-a-delimited-string-into-an-array-in-bash/
-# This undoubtedly invokes some kind of unholy wrath :astonished:
 IFS=', ' read -r -a array <<< "$tags_no_whitespace"
 
 # Write out each role into an Ansible host group
