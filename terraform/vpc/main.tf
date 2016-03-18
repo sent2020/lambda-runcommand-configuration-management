c96eef43f00861008bd1f5fefc6e657bbe42aa3f
Josh Campbell <joshcb@amazon.com>
2016-03-18 14:03:04 -0500

    removes provider, adds enable_dns_hostnames to vpc, adds second subnet, renaming and reshuffling

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,40 +1,52 @@
-provider "aws" {
-  region = "${var.region}"
-}
-resource "aws_vpc" "garlc-vpc" {
+resource "aws_vpc" "garlc_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = "True"
   tags {
     Name = "garlc_vpc"
   }
 }
-resource "aws_subnet" "garlc-subnet" {
-    vpc_id = "${aws_vpc.garlc-vpc.id}"
+output "vpc_id" {
+  value = "${aws_vpc.garlc_vpc.id}"
+}
+
+resource "aws_subnet" "garlc_subnet1" {
+    vpc_id = "${aws_vpc.garlc_vpc.id}"
     cidr_block = "10.0.10.0/24"
     map_public_ip_on_launch = "True"
-    enable_dns_hostnames = "True"
     tags {
-        Name = "garlc_pub_subnet"
+        Name = "garlc_pub_subnet1"
     }
 }
-resource "aws_internet_gateway" "garlc-gateway" {
-    vpc_id = "${aws_vpc.garlc-vpc.id}"
+output "subnet1" {
+  value = "${aws_subnet.garlc_subnet1.id}"
+}
+
+resource "aws_subnet" "garlc_subnet2" {
+    vpc_id = "${aws_vpc.garlc_vpc.id}"
+    cidr_block = "10.0.11.0/24"
+    map_public_ip_on_launch = "True"
+    tags {
+        Name = "garlc_pub_subnet2"
+    }
+}
+output "subnet2" {
+  value = "${aws_subnet.garlc_subnet2.id}"
+}
+
+resource "aws_internet_gateway" "garlc_gateway" {
+    vpc_id = "${aws_vpc.garlc_vpc.id}"
     tags {
         Name = "garlc_igw"
     }
 }
-resource "aws_route_table" "garlc-route" {
-    vpc_id = "${aws_vpc.garlc-vpc.id}"
+
+resource "aws_route_table" "garlc_route" {
+    vpc_id = "${aws_vpc.garlc_vpc.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.garlc-gateway.id}"
+        gateway_id = "${aws_internet_gateway.garlc_gateway.id}"
     }
     tags {
         Name = "garlc_route_table"
     }
-}
-output "vpcid" {
-  value = "${aws_vpc.garlc-vpc.id}"
-}
-output "subnet" {
-  value = "${aws_subnet.garlc-subnet.id}"
 }
