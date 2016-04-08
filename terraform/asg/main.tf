6ef1070bf21e355e822d79c3a58376108dd43565
Josh Campbell <joshcb@amazon.com>
2016-04-08 09:44:06 -0500

    changes subnets to use two subnet variables

--- terraform/asg/main.tf
+++ terraform/asg/main.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size = "${var.max_size}"
   min_size = "${var.min_size}"
   desired_capacity = "${var.desired_capacity}"
-  vpc_zone_identifier = ["${var.subnets}"]
+  vpc_zone_identifier = ["${var.subnet1)}", "${var.subnet2)}"]
   tag {
     key = "Ansible_Roles"
     value = "${var.ansible_roles}"
