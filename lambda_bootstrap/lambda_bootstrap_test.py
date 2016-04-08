c39c1bce6d4be3337b03606a4df2de599c656a32
bchav <bmchavis@gmail.com>
2016-04-07 18:52:30 -0700

    adds a test

--- lambda_bootstrap/lambda_bootstrap_test.py
+++ lambda_bootstrap/lambda_bootstrap_test.py
@@ -5,6 +5,7 @@ from mock import patch, MagicMock
 from lambda_bootstrap import find_bucket
 from aws_lambda_sample_events import SampleEvent
 from botocore.exceptions import ClientError
+from lambda_bootstrap import is_a_garlc_instance
 
 @patch('boto3.client')
 def test_find_bucket(mock_client):
@@ -46,7 +47,7 @@ def test_find_bucket_with_keyerror(mock_client):
 @patch('boto3.client')
 def test_find_bucket_with_clienterror(mock_client):
     """
-    Test the find_bucket functin with a KeyError
+    Test the find_bucket function with a KeyError
     """
     err_msg = {
         'Error': {
@@ -56,3 +57,14 @@ def test_find_bucket_with_clienterror(mock_client):
     }
     mock_client.side_effect = ClientError(err_msg, 'blah')
     assert find_bucket() == False
+
+@patch('boto3.client')
+def test_is_a_garlc_instance(mock_client):
+    """
+    Test is_a_garlc_instance returns true when EC2 API Call
+    returns a string
+    """
+    ec2 = MagicMock()
+    mock_client.return_value = ec2
+    ec2.describe_instances.return_value = "instance"
+    assert is_a_garlc_instance("instance") == True
